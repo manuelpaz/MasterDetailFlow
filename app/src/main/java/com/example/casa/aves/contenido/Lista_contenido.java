0d6ed187eb9abefca83f35d0b2e7d18940a29029
Manuel Paz Robles <manuelpazrobles@gmail.com>
2016-05-02 01:11:51 -0500

    Se realiza la modificación de la clase auxiliar DummyContent que es la que hace posible se muestre contenido tanto en el listado como en el detalle. Al hacer cambios en esta clase se realizan modificaciones en AveDetailFragment y AveListAcivity.

---
+++ app/src/main/java/com/example/casa/aves/contenido/Lista_contenido.java
@@ -0,0 +1,69 @@
+package com.example.casa.aves.contenido;
+
+import com.example.casa.aves.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample textoEncima for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class Lista_contenido {
+
+    /**
+     * Donde se guardan las entradas de la lista.
+     */
+    public static final List<Lista_entrada> ENTRADAS_LISTA = new ArrayList<>();
+
+    /**
+     * Donde se asigna el identificador a cada entrada de la lista
+     */
+    public static final Map<String, Lista_entrada> ENTRADAS_LISTA_HASHMAP = new HashMap<>();
+
+    /**
+     * Creamos estáticamente las entradas
+     */
+    static {
+        aniadirEntrada(new Lista_entrada("0", R.drawable.im_buho, "BUHO", "Búho es el nombre común de aves de la familia Strigidae, del orden de las estrigiformes o aves rapaces nocturnas. Habitualmente designa especies que, a diferencia de las lechuzas, tienen plumas alzadas que parecen orejas."));
+        aniadirEntrada(new Lista_entrada("1", R.drawable.im_colibri, "COLIBRÍ", "Los troquilinos (Trochilinae) son una subfamilia de aves apodiformes de la familia Trochilidae, conocidas vulgarmente como colibríes, quindes, tucusitos, picaflores, chupamirtos, chuparrosas, huichichiquis (idioma nahuatl), mainumby (idioma guaraní) o guanumby. Conjuntamente con las ermitas, que pertenecen a la subfamilia Phaethornithinae, conforman la familia Trochilidae que, en la sistemática de Charles Sibley, se clasifica en un orden propio: Trochiliformes, independiente de los vencejos del orden Apodiformes. La subfamilia Trochilinae incluye más de 100 géneros que comprenden un total de 330 a 340 especies."));
+        aniadirEntrada(new Lista_entrada("2", R.drawable.im_cuervo, "CUERVO", "El cuervo comúnn (Corvus corax) es una especie de ave paseriforme de la familia de los córvidos (Corvidae). Presente en todo el hemisferio septentrional, es la especie de córvido con la mayor superficie de distribución. Con el cuervo de pico grueso, es el mayor de los córvidos y probablemente la paseriforme más pesada; en su madurez, el cuervo común mide entre 52 y 69 centímetros de longitud y su peso varía de 0,69 a 1,7 kilogramos. Los cuervos comunes viven generalmente de 10 a 15 años pero algunos individuos han vivido 40 años. Los juveniles pueden desplazarse en grupos pero las parejas ya formadas permanecen juntas toda su vida, cada pareja defendiendo un territorio. Existen 8 subespecies conocidas que se diferencian muy poco aparentemente, aunque estudios recientes hayan demostrado diferencias genéticas significativas entre las poblaciones de distintas regiones."));
+        aniadirEntrada(new Lista_entrada("3", R.drawable.im_flamenco, "FLAMENCO", "Los fenicopteriformes (Phoenicopteriformes), los cuales reciben el nombre vulgar de flamencos, son un orden de aves neognatas, con un único género viviente: Phoenicopterus. Son aves que se distribuyen tanto por el hemisferio occidental como por el hemisferio oriental: existen cuatro especies en América y dos en el Viejo Mundo. Tienen cráneo desmognato holorrino, con 16 a 20 vértebras cervicales y pies anisodáctilos."));
+        aniadirEntrada(new Lista_entrada("4", R.drawable.im_kiwi, "KIWI", "Los kiwis (Apterix, gr. 'sin alas') son un género de aves paleognatas compuesto por cinco especies endémicas de Nueva Zelanda.1 2 Son aves no voladoras pequeñas, aproximadamente del tamaño de una gallina. Antes de la llegada de los humanos alrededor del año 1300, en Nueva Zelanda los únicos mamíferos que había eran murciélagos, y los nichos ecológicos que en otras partes del mundo eran ocupados por animales tan diversos como caballos, lobos y ratones fueron utilizados en Nueva Zelanda por pájaros (y en menor proporción por ciertas especies de reptiles). La denominación kiwi es maorí, idioma del pueblo homónimo de linaje malayopolinesio que colonizó Nueva Zelanda antes de la llegada de los europeos."));
+        aniadirEntrada(new Lista_entrada("5", R.drawable.im_loro, "LORO", "Las Psitácidas (Psittacidae) son una familia de aves psitaciformes llamadas comúnmente loros o papagayos, e incluye a los guacamayos, las cotorras, los periquitos, los agapornis y formas afines."));
+        aniadirEntrada(new Lista_entrada("6", R.drawable.im_pavo, "PAVO", "Pavo es un género de aves galliformes de la familia Phasianidae, que incluye dos especies, el pavo real común (Pavo cristatus) y el pavo real cuelliverde (Pavo muticus)."));
+    }
+
+    private static void aniadirEntrada(Lista_entrada entrada) {
+        ENTRADAS_LISTA.add(entrada);
+        ENTRADAS_LISTA_HASHMAP.put(entrada.id, entrada);
+    }
+
+
+
+    /**
+     * Representa una entrada del contenido de la lista
+     */
+    public static class Lista_entrada {
+        public final String id;
+        public final int idImagen;
+        public final String textoEncima;
+        public final String textoDebajo;
+
+        public Lista_entrada(String id,int idImagen, String textoEncima, String textoDebajo) {
+            this.id = id;
+            this.idImagen = idImagen;
+            this.textoEncima = textoEncima;
+            this.textoDebajo = textoDebajo;
+        }
+
+        @Override
+        public String toString() {
+            return textoEncima;
+        }
+    }
+}
